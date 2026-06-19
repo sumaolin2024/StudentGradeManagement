19cdee28bb086a82f930e72014215df3962ec508
sumaolin2024 <sumaolin2024@foxmail.com>
2026-06-19 17:44:43 +0800

    初始化学生成绩管理系统 WebForms 项目
    - VS 解决方案 (.sln) + 项目文件 (.csproj)
    - 学生管理 (List/Add/Edit)
    - 课程管理 (List/Add)
    - 成绩录入与查看
    - 统计分析报表
    - SQL Server 数据库初始化脚本
    - 响应式 UI 样式

--- SQLQuery2.sql
+++
@@ -1,9 +0,0 @@
-/****** SSMS 的 SelectTopNRows 命令的脚本  ******/
-SELECT TOP (1000) [StudentNo]
-      ,[Sname]
-      ,[Class]
-      ,[Chinese]
-      ,[Math]
-      ,[English]
-      ,[Computer]
-  FROM [WebDBStudents].[dbo].[Exam]
